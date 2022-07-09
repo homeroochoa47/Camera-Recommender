b13b8ff9d5e4481fdf9056d25f7a148be8b5f5d4
Homero Ochoa <homeroochoa47@gmail.com>
2022-07-09 00:54:07 -0700

    0.2 - updated choice class and traverse function in order to be able to traverse the tree nodes based on user input. Still some work to be done on succesfully doing so.

--- #camera-recommender.py
+++ #camera-recommender.py
@@ -3,49 +3,51 @@
 #while effective, the structure was a but confusing to read and a bit messy, so this time the plan is to make something neater and easier to read. 
 #the plan is to use a tree which starts with a first question and branches out, with the children being the result of certain choices the user makes as they answer questions on what their camera preferences are
 #down the road, it may be interesting to consider some sort of machine learning into the program to better decide what the user might be interested; for now, the program will remain the same in terms of deciding which camera to suggest to the user as the previous version of this program
-    #for now I dont think the amount of choices and cameras for me to recommend is vast enought to need some sort of machine learning-based recommendation, but the idea could be interesting should I choose to try some kind of project based on that down the road
+    #for now I dont think the amount of choices and cameras for me to recommend is vast enought to need some sort of machine learning-based recommendation, but the idea could be interesting should I eventually choose to try some kind of project based on that 
+
 
 
 #choices are nodes which contain the the input function which get called and asks the user to select their camera preferences. Depending on what the input is we need to be able to connect that with the correct child node
-import re
-
-
 class Choice:
 
-    #the suggestion is the assigned topic for this node. e.g. if a user inputs portraits in the last choice, the topic for this choice would be portraits, and we would be able propertly route toward this node based on that input. 
-    def __init__(self, topic = None, input = None):
-        self.topic = topic
-        self.input = input
+    #the actions dictionary is what we use to traverse the nodes and decide which one is the correct next node based on user input. The structure for the actions dictionary is {'topic: [listof topics], 'inputstring': "string", 'recommendation': "camera recommendation"}
+    #Using this structure will make it to where we can ask for an input if an input string exists, and suggest a recommendation if there is no longer a need for user input, meaning we have determined what their recommendation is
+    def __init__(self, actions = None):
+        self.actions = actions #this is a dictionary
         self.children = []
-        self.next_node = None
-
-    def __repr__(self):
-        return ""
         
+    def __repr__(self):
+        if type(self.actions['topic']) == list:
+            return "/".join(self.actions['topic'])
+        else:
+            return self.actions['topic']
+        
+    def get_children(self):
+        return self.children
     
+    #might be able to clean this up
     def add_child(self, child):
-        self.children.append(child)
-    
-    #we might be able to have this function be where we assign an input function to the node instead of instantiating it when the node is created.
-    def request_user_input(self):
-        prompt_input = input(self.input)
-        return prompt_input
+        child_list = self.children
+        child_list.append(child)
+        self.children = child_list
+        return self.children
 
-    #this function will be called in the traverse method to 
-    def select_child(self):
-        for child in children:
-            if self.input == self.topic:
-                self.next_node = child
-        return self.next_node
-
-
+#need to figure out why nothing happens after the first input. The function should move on to the next node and ask for a new input or return a recommendation
 def traverse(start_point):
+    current_node = start_point
     current_children = start_point.children
+    print (start_point.children)
     while len(current_children) > 0:
-        start_point.request_user_input()
-        start_point.select_child()
-         #implement the key:value pair selection w a dictionary
-
+        if 'inputstring' in current_node.actions.keys():
+            userinput = input(current_node.actions['inputstring'])
+        else:
+            return current_node['recommendation']
+        for child in current_children:
+            if userinput.lower() in child.actions['topic']:
+                current_node = child
+                current_children = current_node.children
+            else:
+                pass
          
         
 def show_tree_structure(start_node):
@@ -56,49 +58,52 @@ def show_tree_structure(start_node):
         print(start_node.topic)
         for child in current_children:
             print (f"----->{child.topic}")
-        return show_tree_structure(child)
+            return show_tree_structure(child)
 
             
 first_choice = Choice(
-    [
-        "first_choice"
-    ],
-    "What types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots or maybe a little bit of everything: \n"
+        {
+            'topic': ["first_choice"],
+            'inputstring': "What types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots or maybe a little bit of everything: \n"
+        }
+            
 )
 
 first_choice_child1 = Choice(
-        [
-            "portrait", "landscape"
-        ],
-            "Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses? \n"
+        {
+            'topic': ["portraits", "landscapes"],
+            'inputstring': "Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses? \n"
+        }
 )
 
 first_choice_child2 = Choice(
-        [
-            "friend", "snap"
-        ],
-             "You're probably best off with a point and shoot camera. What's your budget? \n"
+        {
+            'topic': ["friend", "snap"],
+            'inputstring' :"You're probably best off with a point and shoot camera. What's your budget? \n"
+        }
 )
 
-second_choice1 = Choice()
+first_choice_child3 = Choice(
+        {
+            'topic': ["everything"],
+            'inputstring': None,
+            'recommendation':"Your best bet would be the Minolta Hi-Matic 7S."
+            
+        }
+)
+
 
 first_choice.add_child(first_choice_child1)
 first_choice.add_child(first_choice_child2)
+first_choice.add_child(first_choice_child3)
 
 
-
-
-
-print (show_tree_structure(first_choice))
+traverse(first_choice)
 
 
         
 
-#maybe add a traverse method in order to initiate the sequence and traverse through all of the nodes based on the input/script
 #also need to figure out how to more effectively move back up to the past node if we get an input we cant work with as opposed to restarting the while script as with the last program. The nodes may make that a possibility. 
-#this might be a recursive function in which the base case is when we determine a selection (or run out of child nodes)
+    #perhaps a recursive call
 
-#we might be able to use a dictionry to assign values of certain topics/inputs to their corrensponsing follow up question ,or the camera recommendation in a key: value pair
-    #in that case we might be able to combine that with a linked list?
-    
 #at some point we have to figure out what to do when the user submits an invalid input
