a28ec4d1f8fcefc72fd9eca9f7e974b8b13864fb
Homero Ochoa <homeroochoa47@gmail.com>
2022-07-09 20:27:06 -0700

    0.3 - traverse function now operates as intended. Tree nodes all conect and program is able to accept inputs and make a recommendation

--- #camera-recommender.py
+++ #camera-recommender.py
@@ -1,17 +1,14 @@
 #camera-recommender
 #the previous version of this program incorporated a series of functions which called eachother and made suggestions based on the returned value of the functions.
 #while effective, the structure was a but confusing to read and a bit messy, so this time the plan is to make something neater and easier to read. 
-#the plan is to use a tree which starts with a first question and branches out, with the children being the result of certain choices the user makes as they answer questions on what their camera preferences are
-#down the road, it may be interesting to consider some sort of machine learning into the program to better decide what the user might be interested; for now, the program will remain the same in terms of deciding which camera to suggest to the user as the previous version of this program
-    #for now I dont think the amount of choices and cameras for me to recommend is vast enought to need some sort of machine learning-based recommendation, but the idea could be interesting should I eventually choose to try some kind of project based on that 
+#this program is structured as a tree, where we collect inputs starting at the root and move down along the child nodes depending on what the user inputs are. Once we reach a leaf node we return a camera recommendation for the user based on the path their inputs took us down.
 
 
-
-#choices are nodes which contain the the input function which get called and asks the user to select their camera preferences. Depending on what the input is we need to be able to connect that with the correct child node
+#choices are nodes which contain the the input function which get called and asks the user to select their camera preferences. Depending on what the input is we move to the corresponding child node. We search through the topics in the child nodes and match that with the user input to select which node to move to.
 class Choice:
 
     #the actions dictionary is what we use to traverse the nodes and decide which one is the correct next node based on user input. The structure for the actions dictionary is {'topic: [listof topics], 'inputstring': "string", 'recommendation': "camera recommendation"}
-    #Using this structure will make it to where we can ask for an input if an input string exists, and suggest a recommendation if there is no longer a need for user input, meaning we have determined what their recommendation is
+    #Using this structure will make it to where we can ask for an input if an input string exists, and suggest a recommendation if there is no longer a need for user input, meaning we have determined what to recommend them
     def __init__(self, actions = None):
         self.actions = actions #this is a dictionary
         self.children = []
@@ -32,36 +29,50 @@ class Choice:
         self.children = child_list
         return self.children
 
-#need to figure out why nothing happens after the first input. The function should move on to the next node and ask for a new input or return a recommendation
+def request_input(node):
+    userinput = input(node.actions['inputstring'])
+    return userinput
+
+def restart():
+    restart = input('Would you like to start over? ')
+    if "y" in restart.lower():
+        return traverse(root)
+    elif "n" in restart.lower():
+        print ("See ya!")
+        return
+    else:
+        print ("Enter yes or no")
+        return restart()
+
+def give_recommendation(node):
+    print(node.actions['recommendation'])
+    return restart()
+
+
+#function was made to call recursively. Works for now, but will require further refining once all of the tree nodes are added.
 def traverse(start_point):
     current_node = start_point
     current_children = start_point.children
-    print (start_point.children)
-    while len(current_children) > 0:
-        if 'inputstring' in current_node.actions.keys():
-            userinput = input(current_node.actions['inputstring'])
-        else:
-            return current_node['recommendation']
-        for child in current_children:
-            if userinput.lower() in child.actions['topic']:
-                current_node = child
-                current_children = current_node.children
-            else:
-                pass
-         
+    #base case
+    if 'recommendation' in current_node.actions.keys():
+        return give_recommendation(current_node)
+        #call function to get more info or start over
+    
+    #recursive call
+    if 'inputstring' in current_node.actions.keys():
+        userinput = request_input(current_node)
         
-def show_tree_structure(start_node):
-    current_children = start_node.children
-    if len(current_children) == 0:
-        return
-    while len(current_children) > 0:
-        print(start_node.topic)
-        for child in current_children:
-            print (f"----->{child.topic}")
-            return show_tree_structure(child)
-
+    for child in current_children:
+        for topic in child.actions['topic']:       
+            if topic in userinput.lower():
+                return traverse(child)
             
-first_choice = Choice(
+    print ("Sorry, I didn't get that. Please try again.")
+    traverse(current_node)
+
+#maybe add a fourth child in each node that returns to the beginning if the user entered an invalid input
+#need to update node names to clarify the structure of the trees
+root = Choice(
         {
             'topic': ["first_choice"],
             'inputstring': "What types of photos do you want to take? (Portraits, landscapes, pics of your friends, quick snapshots or maybe a little bit of everything: \n"
@@ -69,41 +80,121 @@ first_choice = Choice(
             
 )
 
-first_choice_child1 = Choice(
+#second level
+#---------------------
+#portraits or landscapes
+root_child1 = Choice(
         {
             'topic': ["portraits", "landscapes"],
             'inputstring': "Nice. Do you wand a camera with a fixed lens or do you want to be able to switch lenses? \n"
         }
 )
-
-first_choice_child2 = Choice(
+#snapshots or friends
+root_child2 = Choice(
         {
-            'topic': ["friend", "snap"],
-            'inputstring' :"You're probably best off with a point and shoot camera. What's your budget? \n"
+            'topic': ["friends", "snapshots"],
+            'inputstring' :"You're probably best off with a point and shoot camera. What's your budget? Enter 'a' for under $50, 'b' for $51-$150, or 'c' for $150+ \n"
         }
 )
-
-first_choice_child3 = Choice(
+#everything
+root_child3 = Choice(
         {
             'topic': ["everything"],
-            'inputstring': None,
-            'recommendation':"Your best bet would be the Minolta Hi-Matic 7S."
-            
+            'recommendation':"Your best bet would be the Minolta Hi-Matic 7S." 
         }
 )
+#---------------------
+#under root child 1
+portraits_landscapes_fixed = Choice(
+        {
+        'topic': ['fixed'], 
+        'inputstring': "Fixed lens it is. Now, are you interested in a fully mechanical camera, or one that relies on batteries? \n"
+        }
+)
+#under root child 1
+portraits_landscapes_interchangeable = Choice(
+    {
+        'topic': ['switch', 'lenses'],
+        'inputstring': "So you'd prefer something with interchangeable lenses. Do you want battery powered or fully mechanical? \n"
+    }
+)
+#---------------------
+#under portraits and landscapes -- fixed
+fixed_mechanical = Choice(
+    {
+        'topic': ['mechanical', 'mech'],
+        'recommendation': "You might like the Olympus 35 RC"
+    }
+)
+#under portraits and labdscapes -- fixed
+fixed_battery = Choice(
+    {
+        'topic': ['battery', 'batt'],
+        'recommencation': "You might like the Olympus XA"
+    }
+)
+#---------------------
+#under portraits and landscapes -- interchangeable
+interchangeable_mechanical = Choice(
+    {
+        'topic': ['mechanical', 'mech'],
+        'recommendation': "How about the Nikon FM?"
+    }
+)
+
+interchangeable_battery = Choice(
+    {
+        'topic': ['battery', 'batt'],
+        'recommendation': "How about the Nikon FE?"
+    }
+)
+#---------------------
+#under root child 2
+snapshots_under50 = Choice(
+    {
+        'topic': ['a'],
+        'recommendation': "Check out the Pentax IQZoom"
+    }
+)
+
+snapshots_51_150 = Choice(
+    {
+        'topic': ['b'],
+        'recommendation':  "Check out the Olympus Stylus Zoom"
+    }
+)
+
+snapshots_any_price = Choice(
+    {
+        'topic': ['c'],
+        'recommendation': "Check out the Konica Big Mini"
+    }
+)
+#---------------------
+
+#second level
+root.add_child(root_child1)
+root.add_child(root_child2)
+root.add_child(root_child3)
+
+#third level
+root_child1.add_child(portraits_landscapes_fixed)
+root_child1.add_child(portraits_landscapes_interchangeable)
+
+root_child2.add_child(snapshots_under50)
+root_child2.add_child(snapshots_51_150)
+root_child2.add_child(snapshots_any_price)
+
+#fourth level
+portraits_landscapes_fixed.add_child(fixed_mechanical)
+portraits_landscapes_fixed.add_child(fixed_battery)
+
+portraits_landscapes_interchangeable.add_child(interchangeable_mechanical)
+portraits_landscapes_interchangeable.add_child(interchangeable_battery)
 
 
-first_choice.add_child(first_choice_child1)
-first_choice.add_child(first_choice_child2)
-first_choice.add_child(first_choice_child3)
 
+print ("\nWelcome to the best friend of the modern youth: a tool that helps you figure out what film camera you should buy! What an exciting way to kick off your new hobby.\n")
+print ("Lets start with a few questions to figure out the right choice for you.\n")
+traverse(root)
 
-traverse(first_choice)
-
-
-        
-
-#also need to figure out how to more effectively move back up to the past node if we get an input we cant work with as opposed to restarting the while script as with the last program. The nodes may make that a possibility. 
-    #perhaps a recursive call
-
-#at some point we have to figure out what to do when the user submits an invalid input
